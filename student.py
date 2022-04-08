ed92e12084b07fe5b0e3bf63db26f1f27c12045a
GBrenner1 <gbrenne1@gmail.com>
2022-04-08 19:38:20 -0400

    Create student.py

---
+++ student.py
@@ -0,0 +1,11 @@
+class Student:
+
+    name: str
+    topic: str
+    scores: dict
+
+    def __init__(self) -> None:
+        self.name = ""
+        self.topic = ""
+        self.scores = {}
+        
